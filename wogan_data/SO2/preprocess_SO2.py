04065b6eb0ca9fcf31b77a1e9b01a8e8b603aec3
Nick Wogan <nicholaswogan@gmail.com>
2024-09-02 11:10:14 -0700

    SO2

---
+++ wogan_data/SO2/preprocess_SO2.py
@@ -0,0 +1,39 @@
+import subprocess
+import os
+import shutil
+import zipfile
+
+def main():
+
+    ffile = "09_HITRAN2016.par.zip"
+
+    with zipfile.ZipFile('downloads/'+ffile, 'r') as zip_ref:
+        zip_ref.extractall('extract')
+
+    # copy files to the main directory
+    tmp_files = []
+    for a in os.listdir('extract'):
+        if '.par' in a:
+            if "HITRAN2016" in a:
+                aa = a.replace("HITRAN2016",'hitran16')
+
+            shutil.copy('extract/'+a, '../../'+aa)
+            tmp_files.append(aa)
+
+    # preprocess the files
+    cmd = "./hitran -M 09 -ISO 1 -in hitran16"
+    subprocess.run(cmd.split(), cwd='../../')
+
+    # move processesed data files into data dir
+    for a in os.listdir('../../'):
+        if "hitran16" in a and ".bin" in a:
+            os.rename('../../'+a, "data/"+a)
+        if "hitran16.param" in a:
+            os.rename('../../'+a, "data/"+a)
+    
+    # delete the temporary files
+    for tmp in tmp_files:
+        os.remove('../../'+tmp)
+
+if __name__ == "__main__":
+    main()
